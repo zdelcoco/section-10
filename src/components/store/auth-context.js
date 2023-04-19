2ef61c489bc17190f59ab739630b0c30ec764b0d
Zachary Delcoco <zdelcoco@gmail.com>
2023-04-19 14:38:58 -0500

    module wrap up

---
+++ src/components/store/auth-context.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogOut: () => {},
+  onLogIn: (email, password) => {}
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userStoredIsLoggedIn = localStorage.getItem('isLoggedIn');
+
+    if (userStoredIsLoggedIn === 'true') {
+      setIsLoggedIn(true);
+    };
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.setItem('isLoggedIn','false');
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn','true');
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider 
+      value={{ 
+        isLoggedIn: isLoggedIn, 
+        onLogOut: logoutHandler,
+        onLogIn: loginHandler
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
